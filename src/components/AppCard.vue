0a911a157f6cde5aed9cd5a8aaf3e78d2863ed58
dav100ide <oreglio.dev@gmail.com>
2022-11-30 16:00:27 +0100

    milestone1: prototipo search()

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,10 +1,22 @@
 <script>
 export default {
-   name: 'AppCard',
+   props: {
+      info: Object,
+   },
 };
 </script>
 <template>
-   <div class="ms_card">card</div>
+   <div class="ms_card h-100">
+      <h2>{{ info.title }}</h2>
+      <h3>{{ info.original_title }}</h3>
+      <p>{{ info.original_language }}</p>
+      <div>{{ info.vote_average }}</div>
+   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.ms_card {
+   border: 1px solid black;
+   padding: 0.5rem;
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,39 @@
 <script>
 import AppCard from './AppCard.vue';
+import { store } from '../store';
 export default {
    components: {
       AppCard,
+   },
+   data() {
+      return {
+         store,
+      };
    },
 };
 </script>
 
 <template>
    <main>
-      MAIN:
-      <AppCard v-for="i in 2" />
+      <!-- lista film-->
+      <ul class="movies-list">
+         <li v-for="movie in store.movies">
+            <AppCard :info="movie" />
+         </li>
+      </ul>
+      <!-- /lista film-->
    </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.movies-list {
+   display: flex;
+   flex-wrap: wrap;
+   color: rgb(9, 9, 87);
+   list-style: none;
+   li {
+      width: calc(100% / 3);
+      padding: 10px;
+   }
+}
+</style>
