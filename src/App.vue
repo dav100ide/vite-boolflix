261428c8daa6776ff45a3a758c91115efd838e5b
dav100ide <oreglio.dev@gmail.com>
2022-11-30 14:13:41 +0100

    scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
-  components: {
-    HelloWorld,
-  },
+   components: {
+      AppHeader,
+      AppMain,
+   },
 };
 </script>
 
 <template>
-  <div>
-    <HelloWorld />
-  </div>
+   <div class="container py-1">
+      <AppHeader />
+      <AppMain />
+   </div>
 </template>
 
 <style lang="scss">
-@import "./style/global.scss";
+@import './style/global.scss';
 </style>
