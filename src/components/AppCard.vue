4486955ce7f7b16e7da333be5606bedb7d089c34
dav100ide <oreglio.dev@gmail.com>
2022-11-30 14:29:05 +0100

    AppCard nella cartella components

---
+++ src/components/AppCard.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+   name: 'AppCard',
+};
+</script>
+<template>
+   <div class="ms_card">card</div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,17 @@
-<template>
-   <main>main</main>
-</template>
-
 <script>
-export default {};
+import AppCard from './AppCard.vue';
+export default {
+   components: {
+      AppCard,
+   },
+};
 </script>
+
+<template>
+   <main>
+      MAIN:
+      <AppCard v-for="i in 2" />
+   </main>
+</template>
 
 <style lang="scss" scoped></style>
