16daa8d52690a5beaf29fd5f2d673692078c2a7b
dav100ide <oreglio.dev@gmail.com>
2022-12-01 16:51:08 +0100

    prototipo movieCard & showCard

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,7 +1,26 @@
 <script>
+import CountryFlag from 'vue-country-flag-next';
+
 export default {
    props: {
       info: Object,
+   },
+   components: {
+      CountryFlag,
+   },
+   methods: {
+      getFlag(lang) {
+         if (lang === 'en') {
+            lang = 'gb';
+         } else if (lang === 'ko') {
+            lang = 'kr';
+         } else if (lang === 'zh') {
+            lang = 'cn';
+         } else if (lang === 'ja') {
+            lang = 'jp';
+         }
+         return lang;
+      },
    },
    computed: {
       starNum() {
@@ -16,13 +35,14 @@ export default {
       <h6>{{ info.original_title }}</h6>
       <font-awesome-icon v-for="n in starNum" icon="fa-solid fa-star" />
       <font-awesome-icon v-for="n in 5 - starNum" icon="fa-regular fa-star" />
-      <!-- <img src="" alt=""> bandiera che dice la lingua -->
-      <p>{{ info.original_language }}</p>
+      <img :src="`https://image.tmdb.org/t/p/w342${info.poster_path}`" :alt="info.title" class="img-fluid" />
+      <div><country-flag :country="getFlag(info.original_language)" /></div>
    </div>
 </template>
 
 <style lang="scss" scoped>
 .movie {
+   background-color: #ddd;
    border: 1px solid black;
    padding: 0.5rem;
 }
--- src/components/ShowCard.vue
+++ src/components/ShowCard.vue
@@ -1,7 +1,28 @@
 <script>
+import CountryFlag from 'vue-country-flag-next';
+
 export default {
    props: {
       info: Object,
+   },
+   methods: {
+      getFlag(lang) {
+         if (lang === 'en') {
+            lang = 'gb';
+         } else if (lang === 'ko') {
+            lang = 'kr';
+         } else if (lang === 'zh') {
+            lang = 'cn';
+         } else if (lang === 'ja') {
+            lang = 'jp';
+         }
+         return lang;
+      },
+   },
+   computed: {
+      starNum() {
+         return Math.ceil(this.info.vote_average / 2);
+      },
    },
 };
 </script>
@@ -9,8 +30,10 @@ export default {
    <div class="show h-100">
       <h2>{{ info.name }}</h2>
       <h6>{{ info.original_name }}</h6>
-      <div>{{ info.vote_average }}</div>
-      <p>{{ info.original_language }}</p>
+      <font-awesome-icon v-for="n in starNum" icon="fa-solid fa-star" />
+      <font-awesome-icon v-for="n in 5 - starNum" icon="fa-regular fa-star" />
+      <img :src="`https://image.tmdb.org/t/p/w342${info.poster_path}`" :alt="info.title" class="img-fluid" />
+      <div><country-flag :country="getFlag(info.original_language)" /></div>
    </div>
 </template>
 
